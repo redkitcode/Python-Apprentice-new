64be4652d3674a6c2b5e47e4b3b1fee1139a6ed5
redkitcode <143622684+redkitcode@users.noreply.github.com>
2025-09-15 17:59:19 +0000

    boioioiioioioioioioioing

--- lessons/10_Turtles/30_Turtle_Tricks/10_Turtle_Tricks.py
+++ lessons/10_Turtles/30_Turtle_Tricks/10_Turtle_Tricks.py
@@ -17,6 +17,12 @@ tina = turtle.Turtle()                  # Create a turtle named tina
 # Make each side of the triangle a different color with 
 # tina.pencolor()
 
-... # Your code here
+tina.pendown()
+tina.left(120)
+tina.forward(100)
+tina.left(120)
+tina.forward(100) 
+tina.left(120)
+tina.forward(100)# Your code here
 
 turtle.exitonclick()                    # Close the window when we click on it
