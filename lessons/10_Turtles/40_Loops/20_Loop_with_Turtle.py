3cb5dd555b7cf5d410fea7fa3830aad5d9609999
redkitcode <143622684+redkitcode@users.noreply.github.com>
2025-10-20 18:26:11 +0000

    e

--- lessons/10_Turtles/40_Loops/20_Loop_with_Turtle.py
+++ lessons/10_Turtles/40_Loops/20_Loop_with_Turtle.py
@@ -16,16 +16,16 @@ tina = turtle.Turtle()                  # Create a turtle named tina
 tina.shape('turtle')                    # Set the shape of the turtle to a turtle
 tina.speed(2)                           # Make the turtle move as fast, but not too fast. 
 
-tina.forward(150)                       # Move tina forward by the forward distance
+tina.forward(10)                       # Move tina forward by the forward distance
 tina.left(90)                           # Turn tina left by the left turn
 
-tina.forward(150)                       # Continue the last two steps three more times
+tina.forward(100)                       # Continue the last two steps three more times
 tina.left(90)                           # to draw a square
 
-tina.forward(150)
+tina.forward(100)
 tina.left(90)
 
-tina.forward(150)
+tina.forward(110)
 tina.left(90)
 
 
