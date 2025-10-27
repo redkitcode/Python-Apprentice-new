3e10220aa948eaa5c87a70b7f081896ec60aa249
redkitcode <143622684+redkitcode@users.noreply.github.com>
2025-10-27 17:33:25 +0000

    uhhhhh i have absolutely no idea what to write here

--- lessons/10_Turtles/40_Loops/20_Loop_with_Turtle.py
+++ lessons/10_Turtles/40_Loops/20_Loop_with_Turtle.py
@@ -13,20 +13,12 @@ turtle.setup(600,600,0,0)               # Set the size of the window
 
 tina = turtle.Turtle()                  # Create a turtle named tina
 
-tina.shape('turtle')                    # Set the shape of the turtle to a turtle
-tina.speed(2)                           # Make the turtle move as fast, but not too fast. 
+for i in range(4):
+    tina.shape('turtle')                    # Set the shape of the turtle to a turtle
+    tina.speed(2)                           # Make the turtle move as fast, but not too fast. 
 
-tina.forward(10)                       # Move tina forward by the forward distance
-tina.left(90)                           # Turn tina left by the left turn
-
-tina.forward(100)                       # Continue the last two steps three more times
-tina.left(90)                           # to draw a square
-
-tina.forward(100)
-tina.left(90)
-
-tina.forward(110)
-tina.left(90)
+    tina.forward(100)                       # Move tina forward by the forward distance
+    tina.left(90)                           # Turn tina left by the left turn
 
 
 turtle.exitonclick()                    # Close the window when we click on it
--- lessons/10_Turtles/40_Loops/30_Loop_with_Turtle.py
+++ lessons/10_Turtles/40_Loops/30_Loop_with_Turtle.py
@@ -8,4 +8,19 @@ write a new program that uses a loop to draw a pentagon.
 
 """
 
-... # Your code here
+import turtle                           # Tell Python we want to work with the turtle
+turtle.setup(600,600,0,0)               # Set the size of the window
+
+tina = turtle.Turtle()                  # Create a turtle named tina
+
+angle = 360 / 5                            # Angle to turn for a pentagon
+
+for i in range(5):
+    tina.shape('turtle')                    # Set the shape of the turtle to a turtle
+    tina.speed(2)                           # Make the turtle move as fast, but not too fast. 
+
+    tina.forward(100)                       # Move tina forward by the forward distance
+    tina.left(angle)                        # Turn tina left by the left turn
+
+
+turtle.exitonclick()      # Close the window when we click on it
