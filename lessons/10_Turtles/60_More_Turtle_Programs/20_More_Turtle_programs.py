5eaa7d4f65bd464a8c4084ba744facbd45c068cb
redkitcode <143622684+redkitcode@users.noreply.github.com>
2025-11-03 19:32:00 +0000

    WHAT DID I DOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO

--- lessons/10_Turtles/60_More_Turtle_Programs/20_More_Turtle_programs.py
+++ lessons/10_Turtles/60_More_Turtle_Programs/20_More_Turtle_programs.py
@@ -1,10 +1,24 @@
-"""
-
-Copy the code from the previous lesson, 08a_More_Turtle_programs.ipynb, 
-from the section "Change the Turtle Image"
 
 
-Then change the code so that the turtle has a different image ( look in the 'images'
-directory ) and moves to the corners of the screen in a square pattern. 
-"""
+import turtle as turtle
 
+screen = turtle.Screen()
+screen.setup(width=600, height=600)
+screen.bgcolor('white')
+
+t = turtle.Turtle()
+t.penup()
+t.shape("turtle")
+
+# This is the function that gets called when you click on the screen
+def screen_clicked(x, y):
+    """Print the x and y coordinates of the screen when clicked.
+    and make the turtle move to the clicked location."""
+
+    print('You pressed: x=' + str(x) + ', y=' + str(y))
+
+    t.goto(x, y)
+  
+screen.onclick(screen_clicked) # Important! Tell Python which function to use when the screen is clicked
+
+turtle.done() # Important! Use `done` not `exitonclick` to keep the window open
--- lessons/10_Turtles/70_Projects/10_LeagueBot.py
+++ lessons/10_Turtles/70_Projects/10_LeagueBot.py
@@ -10,11 +10,23 @@ Write your own turtle program! Here is what your program should do
 """
 
 import turtle as turtle
-
 screen = turtle.Screen()
 screen.setup(width=600, height=600)
 screen.bgcolor('white')
+screen.addshape('lessons/10_Turtles/images/leaguebot_bolt.gif')
 
 t = turtle.Turtle()
+t.shape('lessons/10_Turtles/images/leaguebot_bolt.gif')
+t.shapesize(10, 10)
+t.pencolor('blue')
 
-... # Your Code Here
+# Draw hexagon
+sides = 6
+angle = 360 / sides
+distance = 100
+
+for _ in range(sides):
+    t.forward(distance)
+    t.right(angle)
+
+screen.mainloop()
