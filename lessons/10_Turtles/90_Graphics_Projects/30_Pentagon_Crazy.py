0db16cc2c30891a1346d5a57c385c6e56c78f1cb
redkitcode <143622684+redkitcode@users.noreply.github.com>
2026-04-20 18:29:49 +0000

    lfegfwyihgoulifghiruwhbflrwihgbrwilgryllrwguirwhgi

--- lessons/10_Turtles/90_Graphics_Projects/30_Pentagon_Crazy.py
+++ lessons/10_Turtles/90_Graphics_Projects/30_Pentagon_Crazy.py
@@ -23,18 +23,18 @@ myTurtle = turtle.Turtle()
 myTurtle.shape("turtle")
 myTurtle.speed(0)
 myTurtle.width(1)
+myTurtle.speed(999)
 
-sides = 5
-angle = 360 / sides
+sides = 10 # i like 0. 0. 0. 0. but you know what i like more? 8. 8, 8, 8 is great. you can turn it this way you can turn it that way its still 8, 8, 8 is great, i forgot the rest. and yes that was indeed a sesame street reference. i know i know, i was born in the early 2010s, but i still know tons of older media. cry about it
 
-for i in range(360):
+for i in range(1080):
     if i == 100:
         myTurtle.width(2)
     if i == 200:
         myTurtle.width(3)
     myTurtle.pencolor(getNextColor(i))
-    myTurtle.forward(i)
-    myTurtle.right(angle + 1)
+    myTurtle.forward(i/10)
+    myTurtle.right(360/sides + 5)
 
 myTurtle.hideturtle()
 
--- lessons/10_Turtles/90_Graphics_Projects/40_Turtle_Spiral.py
+++ lessons/10_Turtles/90_Graphics_Projects/40_Turtle_Spiral.py
@@ -1,52 +1,48 @@
-"""Penta Spiral
-
-This program already works. See if you can change it to make it draw a different pattern.
+"""hi
+this is a thing that has been created that is currently existing in the universe. it is also an object. did i mention that its a thing? because it is. :D
 
 """
 
-
+#import window
 import random
 import turtle
 
 
-# Returns a random color!
+# give me cooooloooors fancy shmancy dancy in your pantsy. ok fine ill stop.
 def getRandomColor():
     return "#%06X" % (random.randint(0, 0xFFFFFF))
 
-
-window = turtle.Screen()
+window = turtle
+# you should understand this part by now. if you dont, im scared for you.
 window.bgcolor("white")
 
-# Make a new turtle
-myTurtle = turtle.Turtle()
+# make le epik tortol
+tortol = turtle.Turtle()
 
-# This code sets our shape to a turtle
-myTurtle.shape("turtle")
+# this code does what youd expect obviously
+tortol.shape("turtle")
 
-# Set your turtle's speed
-myTurtle.speed(0)
+# do the thingy that makes le tortol get zoomies
+tortol.speed(0)
 
-# Set your turtle's color
-myTurtle.color("green")
+# make tortol blue because it could sing im blue dabadee dabadai dabadee dabadai and it would actually apply to it. why is my brain stuck in the early web despite the fact that i wasnt even born yet... ehh who knows maybe because im a 2000s person at heart. so as i was saying, do the thingy yay woohoo.
+tortol.color("blue")
 
-# Use a loop to repeat the code below 50 times
+# enforce unpaid labor :3 (help im losing it)
 for i in range(50):
 
-    # Set the turtle color to a random color
-    myTurtle.pencolor(getRandomColor())
+    # use those fancy shmancy colors i was talking about earlier. wouldnt want it to go to waste right?
+    tortol.pencolor(getRandomColor())
 
-    # Move the turtle (5*i) pixels. 'i' is the loop variable
-    myTurtle.forward(9 * i)
+    # scooch le tortol. come on, move please. you got this. hey! why are you biting me? ouch! dont do that! somebody get this totrol off of meeeee!
+    tortol.forward(9 * i)
 
-    # Turn the turtle (360/7) degrees to the right
-    myTurtle.right(360 / 7 + i*5)
+    # make le tortol drift even though its not a racecar. well, maybe this tortol is a racecar and im unaware of it...? who knows at this point. oh the unsolved mysteries of life.
+    tortol.right(360 / 2+i)
 
-    # Change the turtle width to 'i' (the loop variable)
-    myTurtle.width(i)
+    # make tortol thicc. tortols arent exactly thin you know.
+    tortol.width(i)
 
-    # Check the pattern against the picture in the recipe. If it matches, you are done!
-
+    # aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee iiiiiiiiiiiiiiiiiiiiiiiiiiii ooooooooooooooooooooooooo uuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuu (vowels are important ok?)
 
 turtle.done()
-
-# Now check in your code!
